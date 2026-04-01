d60adb26598f4aec7ba395543f35d210b8cc8fbb
Quang Tran <tran_thanh_quang@hotmail.com>
2026-04-01 20:49:47 +0700

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }, testInfo) => {
+  await page.goto("https://playwright.dev/");
+
+  // Chụp ảnh màn hình đính kèm vào báo cáo
+  const screenshot = await page.screenshot();
+  await testInfo.attach("Screenshot", {
+    body: screenshot,
+    contentType: "image/png",
+  });
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
+});
